839f0b486fa4c6f692fd2c58cf6c75d4286d8619
jianxionghu <jianxionghu@gmail.com>
2018-04-10 02:26:06 +0800

    Some minor UI improvement

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -35,29 +35,29 @@ class CampaignShow extends Component {
       {
         header: manager,
         description:
-          "众筹发起人(所有者)创建了本众筹合约，它可以发起一个提现请求。",
-        meta: "众筹发起人(所有者)地址",
+          "众筹合约发起人(所有者)创建了本众筹合约，它可以发起一个提现请求。",
+        meta: "众筹合约发起人(所有者)地址",
         style: { overflowWrap: "break-word" }
       },
       {
-        header: minimumContribution,
+        header: minimumContribution + " wei",
         description: "每个参与众筹的人必须投入以下最低资金，并且获取投票权。",
         meta: "最低起投额度 (以wei为单位)"
       },
       {
-        header: requestsCount,
+        header: requestsCount + " 个支付请求",
         description:
           "合约发起人提现请求数量，提现请求必须得到50%投资人的同意，这个钱才能从智能合约里面提到指定地址。",
         meta: "目前已经有的请求付款数量"
       },
       {
-        header: approversCount,
+        header: approversCount+ " 个投资人",
         description:
           "当前出资人的数量，每个出资人都有对提现请求批准的投票权利。",
         meta: "当前出资人的数量"
       },
       {
-        header: web3.utils.fromWei(balance, "ether"),
+        header: web3.utils.fromWei(balance, "ether") + " ETH",
         description: "这个数字代表当前众筹合约的未花费的资金数量。",
         meta: "当前众筹合约拥有的ETH数量（ether）"
       }
